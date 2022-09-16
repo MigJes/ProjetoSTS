8645f76abbff3efdff428d981ca039bd468df552
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-09-15 21:23:13 -0300

    Feat: Adionei o projeto

---
+++ src/main/java/br/com/senac/controller/TurmaController.java
@@ -0,0 +1,38 @@
+package br.com.senac.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import br.com.senac.entity.Turma;
+import br.com.senac.service.TurmaService;
+
+@Controller
+@RequestMapping("turma")
+public class TurmaController {
+	@Autowired
+	private TurmaService turmaService;
+	@GetMapping("/listarTurmas")
+	public ModelAndView listaTodasTurmas() {
+		ModelAndView mv = new ModelAndView("turma/paginaListaTurmas");
+		mv.addObject("turma", turmaService.selectAll());
+		return mv;
+	}
+	
+	@GetMapping("/cadastrarTurma")
+	public ModelAndView cadastrarTurma() {
+		ModelAndView mv = new ModelAndView("turma/cadastrarTurma");
+		mv.addObject("turmanova",new Turma());
+		return mv;
+	}
+	
+	@PostMapping("/salvar")
+	public ModelAndView salvarTurma(Turma turma) {
+		turmaService.insert(turma);
+		return listaTodasTurmas();
+	}
+}
